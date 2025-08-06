255f4677fa3887cf297de59a5dd6b8279621d1c4
LowkeyFract <lowkeyfract@gmail.com>
2025-08-06 10:27:04 +0100

    *

---
+++ src/config/themes.lua
@@ -0,0 +1,26 @@
+local Themes = {
+    Dark = {
+        Name = "Dark",
+        Accent = "#18181b",
+        Dialog = "#18181b",
+        Outline = "#FFFFFF",
+        Text = "#FFFFFF",
+        Placeholder = "#999999",
+        Background = "#0e0e10",
+        Button = "#52525b",
+        Icon = "#a1a1aa",
+    },
+    Light = {
+        Name = "Light",
+        Accent = "#ffffff",
+        Dialog = "#f1f1f1",
+        Outline = "#000000",
+        Text = "#000000",
+        Placeholder = "#666666",
+        Background = "#ffffff",
+        Button = "#d4d4d8",
+        Icon = "#52525b",
+    }
+}
+
+return Themes
--- src/main.lua
+++ src/main.lua
@@ -17,6 +17,7 @@ local function loadFileFromGitHub(path)
     end
 end
 
+local Themes = loadFileFromGitHub("config/themes")
 local KeyAPI = loadFileFromGitHub("modules/KeyAPI")
 local LoadingScreen = loadFileFromGitHub("modules/LoadingScreen")
 
@@ -47,31 +48,8 @@ LoadingScreen:ShowAsync()
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 local WindUiSettings = {
-    SelectedTheme = "Dark",
-    Themes = {
-        Dark = {
-            Name = "Dark",
-            Accent = "#18181b",
-            Dialog = "#18181b",
-            Outline = "#FFFFFF",
-            Text = "#FFFFFF",
-            Placeholder = "#999999",
-            Background = "#0e0e10",
-            Button = "#52525b",
-            Icon = "#a1a1aa",
-        },
-        Light = {
-            Name = "Light",
-            Accent = "#ffffff",
-            Dialog = "#f1f1f1",
-            Outline = "#000000",
-            Text = "#000000",
-            Placeholder = "#666666",
-            Background = "#ffffff",
-            Button = "#d4d4d8",
-            Icon = "#52525b",
-        }
-    }
+    SelectedTheme = "Light",
+    Themes = Themes
 }
 
 for _, themeData in pairs(WindUiSettings.Themes) do
@@ -158,7 +136,7 @@ local function LicenseWindow_F()
                     Icon = "info",
                     Content = licensedata.message .. " Loading hub.."
                 })
-            LicenseWindow:Close():Destroy(task.wait(6), MainWindow_F())
+            Elements.LicenseWindow:Close():Destroy(task.delay(4, function(MainWindow_F) end))
             writefile(LicenseWindow.Folder .. "/" .. "license" .. ".key", tostring(License))
             end
         end
