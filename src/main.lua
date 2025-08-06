c88dcecf5701afa715fc68ab8e4b04c71da6e295
LowkeyFract <lowkeyfract@gmail.com>
2025-08-06 09:31:51 +0100

    v0.001

--- src/main.lua
+++ src/main.lua
@@ -89,7 +89,7 @@ local function LicenseWindow_F()
         Title = "Fractxlware Public",
         Icon = "rbxassetid://129260712070622",
         IconThemed = true,
-        Author = "Made by LowkeyFract",
+        Author = "v0.03",
         Folder = "Fractxlware",
         Size = UDim2.fromOffset(580, 460),
         Transparent = true,
@@ -101,6 +101,11 @@ local function LicenseWindow_F()
         },
         SideBarWidth = 200,
         ScrollBarEnabled = true,
+
+        Elements.LicenseWindow:DisableTopbarButtons({
+            "Minimize",
+            "Fullscreen",
+        })
     })
 
     Elements.License = LicenseWindow:Tab({
