accc52607eec637e91ac2f4ad99ab8f73ad40ac0
LowkeyFract <LowkeyFract@gmail.com>
2025-08-07 10:13:23 +0100

    Fixed ScriptHub

--- src/config/scripthub.lua
+++ src/config/scripthub.lua
@@ -3,13 +3,13 @@ local scripthub = {
         Name = "Skido Dahood Menu",
         Icon = "chevrons-left-right-ellipsis",
         IconThemed = true,
-        loadstring = loadstring(game:HttpGet("https://raw.githubusercontent.com/yuvic123/SKIDO-V3/refs/heads/main/%40Siom4i_"))()
+        ScriptUrl = "https://raw.githubusercontent.com/yuvic123/SKIDO-V3/refs/heads/main/%40Siom4i_"
     },
     Template2 = {
         Name = "Template 2",
         Icon = "chevrons-left-right-ellipsis",
         IconThemed = true,
-        loadstring = ""
+        ScriptUrl = ""
     },
 }
 return scripthub
